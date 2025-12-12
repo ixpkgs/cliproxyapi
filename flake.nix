32910d356b8b198ef1907671cda6886afb48f316
aiwao <233898056+aiwao@users.noreply.github.com>
2025-12-12 20:09:31 +0800

    Package CLIProxyAPI v6.6.7-0
    Upstream commit: ba6aa5fbbe00d64efdfdf621c14aa6b4e10e1c5a

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          tag = "v6.6.7";
-          commit = "1c52a89535d0fbce77ac8b58d93e53bd16cff835";
+          tag = "v6.6.7-0";
+          commit = "ba6aa5fbbe00d64efdfdf621c14aa6b4e10e1c5a";
         in
         {
           default = pkgs.buildGoModule {
@@ -28,7 +28,7 @@
               rev = commit;
             };
 
-            vendorHash = "sha256-kF5VyRyrIza+QrTcuNQZznTrBZ3Pf1QTOJi9Twby6j4=";
+            vendorHash = "sha256-au2/wdcsW1cZNEtTUt32LtnBXME6mTHHHgd7SoSbg74=";
             subPackages = [ "cmd/server" ];
 
             ldflags = [
