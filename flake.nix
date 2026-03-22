01b271fcb2d6404c58f3e1455da2408d35db76a7
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-22 21:19:46 +0800

    Package CLIProxyAPI v0.1.0
    Upstream commit: 768ffe227297312880fd3eaa65c8b37aeaeaefe2

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tag = "v0.1.0";
+          commit = "768ffe227297312880fd3eaa65c8b37aeaeaefe2";
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "cliproxyapi";
+            version = nixpkgs.lib.removePrefix "v" tag;
+
+            src = builtins.fetchGit {
+              url = "https://github.com/router-for-me/CLIProxyAPI";
+              rev = commit;
+            };
+
+            vendorHash = "sha256-3h68+GSEvd7tcJOqTjV2KXBXZFX7AWg3r8K3zZe4DnI=";
+            subPackages = [ "cmd/server" ];
+
+            ldflags = [
+              "-s"
+              "-w"
+              "-X=main.Version=${tag}"
+              "-X=main.Commit=${commit}"
+            ];
+
+            postInstall = ''
+              mv "$out/bin/server" "$out/bin/cli-proxy-api"
+            '';
+
+            meta = {
+              description = "Proxy server providing OpenAI-compatible APIs for CLI clients";
+              homepage = "https://github.com/router-for-me/CLIProxyAPI";
+              license = nixpkgs.lib.licenses.mit;
+              mainProgram = "cli-proxy-api";
+            };
+          };
+        });
+    };
+}
