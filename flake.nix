3c714abb73b1b39bc5714eb41fcfaddd3de2a8f7
aiwao <233898056+aiwao@users.noreply.github.com>
2026-06-13 03:57:29 +0800

    Package CLIProxyAPI v7.1.71
    Upstream commit: b6c22f2d827bc3ad708184bfd831eb21790f8637

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          tag = "v7.1.70";
-          commit = "44d3066a9c9a004c99fd27ad301f77d1099eee35";
+          tag = "v7.1.71";
+          commit = "b6c22f2d827bc3ad708184bfd831eb21790f8637";
         in
         {
           default = pkgs.buildGoModule {
