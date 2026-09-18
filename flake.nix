39a1ace44b119466fd601b426ca84f841f4ccd3e
ixpkgs <ixpkgs@140.lol>
2026-09-19 05:42:42 +0900

    Package CLIProxyAPI v7.2.5
    Upstream commit: bbef8da454c88ad09d6e589f7ddce5ed2eeddb51

--- flake.nix
+++ flake.nix
@@ -17,9 +17,9 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          tag = "v7.2.4";
-          version = "7.2.4";
-          commit = "f33bc56bb947134e4fc10bdd3212404c56dcbaf8";
+          tag = "v7.2.5";
+          version = "7.2.5";
+          commit = "bbef8da454c88ad09d6e589f7ddce5ed2eeddb51";
         in
         {
           default = pkgs.buildGoModule {
