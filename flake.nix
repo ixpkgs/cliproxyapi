ef084367e6a32359db49c407175df243cd53cc5c
ixpkgs <ixpkgs@140.lol>
2026-09-19 04:58:28 +0900

    Package CLIProxyAPI v7.1.19
    Upstream commit: 21fad9dbb447a2ab70d51d0ac3e3d032525a6054

--- flake.nix
+++ flake.nix
@@ -17,9 +17,9 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          tag = "v7.1.18";
-          version = "7.1.18";
-          commit = "a726e373941517795155d9076710d7498e16f1a6";
+          tag = "v7.1.19";
+          version = "7.1.19";
+          commit = "21fad9dbb447a2ab70d51d0ac3e3d032525a6054";
         in
         {
           default = pkgs.buildGoModule {
@@ -30,6 +30,11 @@
               url = "https://github.com/router-for-me/CLIProxyAPI";
               rev = commit;
             };
+
+            postPatch = pkgs.lib.optionalString (version == "7.1.19") ''
+              substituteInPlace sdk/cliproxy/auth/request_auth_prepare_test.go \
+                --replace-fail "/CLIProxyAPI/v6/" "/CLIProxyAPI/v7/"
+            '';
 
             vendorHash = "sha256-AIue9XBsfsKGClRLB1DCME+36crapnOdQrEICFYG1a0=";
             subPackages = [ "cmd/server" ];
